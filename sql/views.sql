875c76a89187db07f09bb72e3512048760dd47c6
DanielBorbafs <dev.danielborba@gmail.com>
2025-05-19 21:45:27 -0300

    news querys e views

--- sql/views.sql
+++ sql/views.sql
@@ -13,3 +13,25 @@ SELECT
     (SELECT SUM(SALARIO) FROM funcionarios) +
     (SELECT SUM(SALARIO) FROM medicos) + 
     (SELECT SUM(VALOR) FROM gastos AS TOTAL_GASTO);
+
+
+
+-- View retorna o lucro total por especialidade
+CREATE VIEW view_consulta_lucro_por_especialidade as
+SELECT M.ESPECIALIDADE, SUM(C.VALOR) AS LUCRO
+FROM medicos M 
+INNER JOIN CONSULTAS C ON C.ID_MEDICO = M.ID_MEDICO
+GROUP BY M.ESPECIALIDADE
+ORDER BY LUCRO
+
+
+-- Essa view retorna todo o gasto da clinica ate o momento
+CREATE VIEW view_gasto_total as
+SELECT SUM(VALOR) AS TOTAL_GASTO 
+FROM gastos
+
+-- VIEW PARA TRAZER TODO O LUCRO ATÉ O MOMENTO
+CREATE VIEW view_lucro_total AS
+SELECT SUM(VALOR) AS LUCRO
+FROM CONSULTAS 
+
